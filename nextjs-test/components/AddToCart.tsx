8a68baa358e850e153c2c4f24293cc6c203443eb
paikpaik <woduf9311@gmail.com>
2023-12-07 00:28:11 +0900

    Add: Routing, Navigation, CSR, SSR, Data Fetching

---
+++ nextjs-test/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("AddToCart")}>Add</button>
+    </div>
+  );
+};
+
+export default AddToCart;
